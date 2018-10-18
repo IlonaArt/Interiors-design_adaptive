c11f8108e1ee519c9c06198e1c7ee67a5d24352c
IlonaArt <ilonasurdream@gmail.com>
2018-10-19 00:08:14 +0300

    Added slick-slider

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ menu.addEventListener("click", function (evt) {
     evt.preventDefault();
     navList.classList.toggle("show-nav");
 });
+
+$(document).ready(function(){
+  $('.slider-box2').slick({
+    arrows: false,
+    dots: true,
+    responsive: [{
+      breakpoint:2000,
+      settings: "slick"
+    },
+    {
+      breakpoint:4000,
+      settings: "unslick"
+    }]
+  });
+  $('.slider-box1').slick({
+    arrows: false,
+    dots: true,
+    responsive: [{
+      breakpoint:2000,
+      settings: "slick"
+    },
+    {
+      breakpoint:4000,
+      settings: "unslick"
+    }]
+  });
+});
